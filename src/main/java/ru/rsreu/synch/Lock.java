11651fbdac81290f7165f5abd337d4446014ee1d
Rudi <rudi.will.do.it@gmail.com>
2023-11-06 10:24:06 +0300

    fix

--- src/main/java/ru/rsreu/synch/Lock.java
+++ src/main/java/ru/rsreu/synch/Lock.java
@@ -3,7 +3,7 @@ package ru.rsreu.synch;
 public class Lock {
     private boolean isLocked = false;
 
-    private static final Object LOCK = new Object();
+    private final Object LOCK = new Object();
 
     public void lock() throws InterruptedException {
         synchronized(LOCK) {
